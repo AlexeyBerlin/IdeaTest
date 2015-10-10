87e49c4d5a0b7b612a3a7195b20e84b13b733ffb
berlinisimus <berlinisimus.92@gmail.com>
2015-10-10 17:02:10 +0300

    added file

---
+++ geekhub/src/geekhub1/Main.java
@@ -0,0 +1,11 @@
+package geekhub1;
+
+/**
+ * Created by Alex on 10/10/2015.
+ */
+public class Main {
+    public static void main(String args[]){
+        System.out.println("Hello 10");
+    }
+
+}
